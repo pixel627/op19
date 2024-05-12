0d01a14db9565b1585a68f0ebc18d7c4cb3a8f3c
pixel627 <pixelguy627@gmail.com>
2024-05-12 23:20:17 +0300

    saveFileWithMathematicalExpression

--- libs/file_io/file_io.c
+++ libs/file_io/file_io.c
@@ -10,6 +10,7 @@ size_t readFileToBuff(char *filePath, char *buff, size_t buffSize) {
     size_t size = fread(buff, sizeof(char), buffSize, f);
     fclose(f);
 
+    buff[size] = '\0';
     return size;
 }
 
@@ -24,6 +25,10 @@ size_t exponentialNumToNum(char *file_path_in, char *file_path_out) {
         exit(1);
     }
 
+    if (fd == NULL) {
+        fprintf(stderr, "file cannot be opened");
+        exit(1);
+    }
     size_t counter = 0;
 
     float val;
@@ -36,7 +41,78 @@ size_t exponentialNumToNum(char *file_path_in, char *file_path_out) {
     return counter;
 }
 
-//
+int processOperation(int d1, int d2, char op) {
+    switch (op) {
+        case '+' :
+            return d1 + d2;
+            break;
+        case '-' :
+            return d1 - d2;
+            break;
+        case '/' :
+            return d1 / d2;
+            break;
+        case '*' :
+            return d1 * d2;
+            break;
+    }
+
+    return 0;
+}
+
+size_t saveFileWithMathematicalExpression(char *file_path_in, char *file_path_out) {
+    FILE *fp = fopen(file_path_in, "r");
+    FILE *fd = fopen(file_path_out, "a+");
+
+    if (fp == NULL) {
+        fprintf(stderr, "file cannot be opened");
+        exit(1);
+    }
+    if (fd == NULL) {
+        fprintf(stderr, "file cannot be opened");
+        exit(1);
+    }
+
+    int countRes = 0;
+
+    int d1, d2, d3;
+    char op1, op2;
+    int res;
+    BagOfWords words;
+    while (fgets(_fileReadBuffer, sizeof(_fileReadBuffer), fp) != NULL) {
+        clearBagOfWords(&words);
+        getBagOfWords(&words, _fileReadBuffer);
+
+        if (words.size == 3) {
+            d1 = *words.words[0].begin - '0';
+            d2 = *words.words[2].begin - '0';
+            op1 = *words.words[1].begin;
+            res = processOperation(d1, d2, op1);
+        } else if (words.size == 5) {
+            d1 = *words.words[0].begin - '0';
+            d2 = *words.words[2].begin - '0';
+            d3 = *words.words[4].begin - '0';
+            op1 = *words.words[1].begin;
+            op2 = *words.words[3].begin;
+
+            if (op1 == '*' || op1 == '/') {
+                res = processOperation(d1, d2, op1);
+                res = processOperation(res, d3, op2);
+            } else {
+                res = processOperation(d2, d3, op2);
+                res = processOperation(d1, res, op1);
+            }
+        }
+
+        countRes++;
+        fprintf(fd,"%d\n", res);
+    }
+
+    fclose(fp);
+    fclose(fd);
+
+    return countRes;
+}
 
 
 int isPatternInWord(WordDescriptor word, const char *pattern) {
@@ -66,7 +142,10 @@ size_t saveFileWithRequiredLen(char *file_path_in, char *file_path_out, char *pa
         fprintf(stderr, "file cannot be opened");
         exit(1);
     }
-
+    if (fd == NULL) {
+        fprintf(stderr, "file cannot be opened");
+        exit(1);
+    }
     size_t counter = 0;
     BagOfWords words;
     while (fgets(_fileReadBuffer, sizeof(_fileReadBuffer), fp) != NULL) {
@@ -94,6 +173,10 @@ size_t saveFileWithLongestWord(char *file_path_in, char *file_path_out) {
     FILE *fp = fopen(file_path_in, "r");
     FILE *fd = fopen(file_path_out, "w+");
     if (fp == NULL) {
+        fprintf(stderr, "file cannot be opened");
+        exit(1);
+    }
+    if (fd == NULL) {
         fprintf(stderr, "file cannot be opened");
         exit(1);
     }
